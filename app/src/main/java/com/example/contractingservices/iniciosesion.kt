5b13619a0e992ffb1646e5cef9019a0a72c4aead
stefania2507 <espinosa4484@gmail.com>
2021-05-27 21:36:44 -0500

    cambios stefania

--- app/src/main/java/com/example/contractingservices/iniciosesion.kt
+++ app/src/main/java/com/example/contractingservices/iniciosesion.kt
@@ -1,6 +1,5 @@
 package com.example.contractingservices
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.inicio.*
